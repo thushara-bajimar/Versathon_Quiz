d02ccc8bf878738935c23219bf000214cf79c865
Thushara B S <abhibajimar07@gmail.com>
2026-02-14 21:24:28 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 #include<vector>
 using namespace std;
 
-void menu();
+void menu(){
+    string username;
+    int choice;
+
+    cout<< " ***** Welcome To YOUR QUIZ PLATFORM !!! *****" << endl;
+    cout<< "Enter your USERNAME: ";
+    cin>> username;
+
+    cout<< "1. Start Quiz"<<endl;
+    cout<< "2. Show Leaderboard"<<endl;
+    cout<< "Enter your choice: ";
+    cin>> choice;
+
+    if(choice == 1){
+        quiz(username);
+    }else if(choice == 2){
+        leaderboard();
+    }
+}
+
 void quiz(string username);
-void score();
+void leaderboard();
 
 int main(){
     menu();
